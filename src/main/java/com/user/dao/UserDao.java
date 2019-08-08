c4a1d5bcf5cc3734268b18fd00787ba37de29432
SambaLim <limsungho07@hanmail.net>
2019-08-08 10:04:25 +0900

    userdao add func

--- src/main/java/com/user/dao/UserDao.java
+++ src/main/java/com/user/dao/UserDao.java
@@ -1,5 +1,34 @@
 package com.user.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.user.domain.User;
+
 public class UserDao {
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/toby", "root", "rnd12345");
+		
+		PreparedStatement ps = c.prepareStatement(
+				"INSERT INTO users(id, name, password) values(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+		
+	}
 	
+	public User get(String Id) {
+		
+		return null;
+	}
 }
