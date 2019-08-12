24c56bec0d4ea01057aa382814f65f10877dcbc2
SambaLim <limsungho07@hanmail.net>
2019-08-12 22:02:43 +0900

    DaoFactory get annotation

--- src/main/java/com/user/dao/DaoFactory.java
+++ src/main/java/com/user/dao/DaoFactory.java
@@ -1,11 +1,22 @@
 package com.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+	
+	@Bean
 	public UserDao userDao() {
 		
 		ConnectionMaker connectionMaker = new DConnectionMaker();
 		UserDao userDao = new UserDao(connectionMaker);
 		
 		return userDao;
+	}
+	
+	@Bean
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
 	}
 }
