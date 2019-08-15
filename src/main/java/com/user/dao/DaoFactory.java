7b8d378e434f10f23e1b5c1e5d3e5c9b30602d15
SambaLim <limsungho07@hanmail.net>
2019-08-15 21:46:54 +0900

    ConnectionMaker to DataSource2

--- src/main/java/com/user/dao/DaoFactory.java
+++ src/main/java/com/user/dao/DaoFactory.java
@@ -1,7 +1,10 @@
 package com.user.dao;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 public class DaoFactory {
@@ -10,8 +13,7 @@ public class DaoFactory {
 	public UserDao userDao() {
 		
 		UserDao userDao = new UserDao();
-		
-		//TODO: setDataSource
+		userDao.setDataSource(dataSource());
 		
 		return userDao;
 	}
@@ -24,5 +26,17 @@ public class DaoFactory {
 	@Bean
 	public ConnectionMaker realConnectionMaker() {
 		return new DConnectionMaker();
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mysql://localhost:3306/toby?serverTimezone=UTC");
+		dataSource.setUsername("root");
+		dataSource.setPassword("rnd12345");
+		
+		return dataSource;
 	}
 }
