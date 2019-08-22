6409ec95f32ac1e5ce8ea3a2c2a2c9ac53bde19a
SambaLim <limsungho07@hanmail.net>
2019-08-22 22:14:58 +0900

    upd testcode

--- src/main/java/com/user/dao/UserDaoTest.java
+++ src/main/java/com/user/dao/UserDaoTest.java
@@ -19,22 +19,47 @@ public class UserDaoTest {
 		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 		
 		UserDao dao = context.getBean("userDao", UserDao.class);
-		User user = new User();
+		User user1 = new User("samba", "임성호", "lim");
+		User user2 = new User("sound", "이소리", "lee");
 		
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 		
-		user.setId("amazing");
-		user.setName("임성호");
-		user.setPassword("lim");
+		dao.add(user1);
+		dao.add(user2);
 		
-		dao.add(user);
+		assertThat(dao.getCount(), is(2));
 		
+		User userget1 = dao.get(user1.getId());
+		User userget2 = dao.get(user2.getId());
+		
+		assertThat(userget1.getName(), is(user1.getName()));
+		assertThat(userget1.getPassword(), is(user1.getPassword()));
+		
+		assertThat(userget2.getName(), is(user2.getName()));
+		assertThat(userget2.getPassword(), is(user2.getPassword()));
+	}
+	
+	@Test
+	public void count() throws SQLException, ClassNotFoundException {
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		User user1 = new User("samba", "임성호", "lim");
+		User user2 = new User("sound", "이소리", "lee");
+		User user3 = new User("amazing", "킹", "king");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user1);
 		assertThat(dao.getCount(), is(1));
 		
-		User user2 = dao.get(user.getId());
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
 		
-		assertThat(user2.getName(), is(user.getName()));
-		assertThat(user2.getPassword(), is(user.getPassword()));
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
 	}
 }
