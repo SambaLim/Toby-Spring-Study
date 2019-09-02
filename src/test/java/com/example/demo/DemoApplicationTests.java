339fb98cf5b0ab16eac686f1164b201d8cb04ff1
SambaLim <limsungho07@hanmail.net>
2019-09-02 22:06:40 +0900

    dirtiesContext

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -40,8 +43,8 @@ public class DemoApplicationTests {
 		
 		this.context = new AnnotationConfigApplicationContext(DaoFactory.class);
 		
-		System.out.println(this.context);
-		System.out.println(this);
+		DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost:3306/toby?serverTimezone=UTC", "root", "rnd12345", true);
+		dao.setDataSource(dataSource);
 		
 		this.user1 = new User("samba", "임성호", "lim");
 		this.user2 = new User("sound", "이소리", "lee");
