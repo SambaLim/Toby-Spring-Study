753aa9bac3aa06062a6c47aa1c156998340a4f75
SambaLim <limsungho07@hanmail.net>
2019-09-09 22:18:47 +0900

    count try catch finally

--- src/main/java/com/user/dao/UserDao.java
+++ src/main/java/com/user/dao/UserDao.java
@@ -97,20 +97,43 @@ public class UserDao {
 	}
 	
 	public int getCount() throws SQLException {
-		Connection c = dataSource.getConnection();
+		Connection c = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		
-		PreparedStatement ps = c.prepareStatement(
-				"SELECT COUNT(*) FROM users");
-		
-		ResultSet rs = ps.executeQuery();
-		rs.next();
-		
-		int count = rs.getInt(1);
-		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		return count;
+		try {
+			c = dataSource.getConnection();
+			
+			ps = c.prepareStatement(
+					"SELECT COUNT(*) FROM users");
+			
+			rs = ps.executeQuery();
+			rs.next();
+			return rs.getInt(1);
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch(SQLException e) {
+					
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch(SQLException e) {
+					
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch(SQLException e) {
+					
+				}
+			}
+		}
 	}
 }
