a0ee4b039e02dc04c55cbe8f8af013dfb5e212c8
SambaLim <limsungho07@hanmail.net>
2019-09-12 10:48:00 +0900

    makeStatement

--- src/main/java/com/user/dao/UserDao.java
+++ src/main/java/com/user/dao/UserDao.java
@@ -72,8 +72,7 @@ public class UserDao {
 		
 		try {
 			c = dataSource.getConnection();
-			ps = c.prepareStatement(
-					"DELETE FROM users");
+			ps = makeStatement(c);
 			ps.executeUpdate();
 		} catch(SQLException e) {
 			throw e;
@@ -94,6 +93,13 @@ public class UserDao {
 				} 
 			}
 		}
+	}
+	
+	private PreparedStatement makeStatement(Connection c) throws SQLException {
+		PreparedStatement ps;
+		ps = c.prepareStatement(
+				"DELETE FROM users");
+		return ps;
 	}
 	
 	public int getCount() throws SQLException {
