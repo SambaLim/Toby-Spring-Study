0559bc23201e1edb692ace620abdaacb0e2df627
SambaLim <limsungho07@hanmail.net>
2019-09-17 21:41:01 +0900

    new deleteAll

--- src/main/java/com/user/dao/UserDao.java
+++ src/main/java/com/user/dao/UserDao.java
@@ -67,36 +67,8 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		
-		try {
-			
-			c = dataSource.getConnection();
-			
-			StatementStrategy strategy = new DeleteAllStatement();
-			ps = strategy.makePreparedStatement(c);
-			ps.executeUpdate();
-			
-		} catch(SQLException e) {
-			throw e;
-		} finally {
-			if(ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-					
-				} 
-			}
-			
-			if(c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-					
-				} 
-			}
-		}
+		StatementStrategy st = new DeleteAllStatement();
+		jdbcContextWithStatementStrategy(st);
 	}
 	
 	public int getCount() throws SQLException {
@@ -123,6 +95,36 @@ public class UserDao {
 					
 				}
 			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch(SQLException e) {
+					
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch(SQLException e) {
+					
+				}
+			}
+		}
+	}
+	
+	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		
+		try {
+			c = dataSource.getConnection();
+			ps = stmt.makePreparedStatement(c);
+			
+			ps.executeUpdate();
+			
+		} catch(SQLException e) {
+			throw e;
+		} finally {
 			if (ps != null) {
 				try {
 					ps.close();
