0cd3bdad87ce62585abe6c5b58623ef9a1c27206
SambaLim <limsungho07@hanmail.net>
2019-09-30 22:19:20 +0900

    template callback pattern

--- src/main/java/com/user/dao/UserDao.java
+++ src/main/java/com/user/dao/UserDao.java
@@ -71,13 +71,7 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-				PreparedStatement ps = c.prepareStatement(
-						"DELETE FROM users");
-				return ps;
-			}
-		});
+		executeSql("DELETE FROM users");
 	}
 	
 	public int getCount() throws SQLException {
@@ -119,6 +113,15 @@ public class UserDao {
 				}
 			}
 		}
+	}
+	
+	private void executeSql(final String query) throws SQLException {
+		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+				PreparedStatement ps = c.prepareStatement(query);
+				return ps;
+			}
+		});
 	}
 	
 	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
